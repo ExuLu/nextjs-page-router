b3df38b8bbffc8a254ad4d889314a77359f7a2d6
Alena Sharai <exultantislupus@gmail.com>
2025-10-28 17:43:42 +0000

    feat: add Cabin view to the cabin page

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { getCabin } from '@/lib/data-service';
+import CabinView from '@/components/CabinView';
 
 export async function getServerSideProps({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -18,7 +19,9 @@ const Cabin = ({ cabin }) => {
         <title>Cabin {router.query.cabinId} | The Wild Oasis</title>
       </Head>
 
-      <div>Cabin #{router.query.cabinId}</div>
+      <div className='max-w-6xl mx-auto mt-8'>
+        <CabinView cabin={cabin} />
+      </div>
     </>
   );
 };
