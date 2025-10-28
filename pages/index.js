90b76af34975636676b2bc1b1af02473f1d8fb67
Alena Sharai <exultantislupus@gmail.com>
2025-10-28 14:30:22 +0000

    feat: add links from home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Hello Pages Router</h1>;
+  return (
+    <div>
+      <h1>Hello Pages Router</h1>
+
+      <div>
+        <Link href='/cabins'>Cabins</Link>
+        <Link href='/contact'>Contact us</Link>
+        <Link href='/about'>About us</Link>
+      </div>
+    </div>
+  );
 }
