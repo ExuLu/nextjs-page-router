35c981fb7e0df147d2c3ae5836fa3db9b043f4cb
Alena Sharai <exultantislupus@gmail.com>
2025-10-28 13:21:29 +0000

    chores: add envs and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '<YOUR URL HERE>',
+        hostname: 'https://rbxbooyeremafggkuigp.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/cabin-images/**',
       },
